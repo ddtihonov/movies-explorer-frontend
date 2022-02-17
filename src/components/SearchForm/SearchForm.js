cb9b2cfa606bd121228064c2660cd59d51b8ed93
Dmitry <ddtihonov@yandex.ru>
2022-02-18 02:40:46 +0300

    read

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SearchForm.css'
+
+export default function SearchForm () {
+
+    
+    return (
+        <section className='search'>
+            <div className='search__container'>
+            
+            </div>
+        </section>
+    )
+}
